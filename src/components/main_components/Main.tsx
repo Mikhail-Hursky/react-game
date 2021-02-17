2544973b7360c22bcd8aa87568a95eb6e5e71305
Mikhail Hursky <kakai747o@gmail.com>
2021-02-17 15:45:20 +0300

    feat: add Main

---
+++ src/components/main_components/Main.tsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import '../../styles/main/Main.scss'
+
+export default class Header extends React.Component {
+    constructor(props: {} | Readonly<{}>) {
+        super(props);
+    }
+
+    render() {
+        return <main className="Main">
+
+        </main>;
+    }
+}
