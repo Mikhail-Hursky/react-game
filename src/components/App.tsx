bf7f455c2990494783385c81dd2a80102b097e6f
Mikhail Hursky <kakai747o@gmail.com>
2021-02-17 15:46:25 +0300

    feat: add Card

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -1,10 +1,17 @@
 import React from 'react';
-import '../styles/App.css';
+import '../styles/App.scss';
+import Header from "./header_components/Header";
+import Main from "./main_components/Main";
+import Footer from "./footer_components/Footer";
 
 function App() {
-  return (
-    <div className="App" />
-  );
+    return (
+        <div className="App">
+            <Header/>
+            <Main/>
+            <Footer/>
+        </div>
+    );
 }
 
 export default App;
---
+++ src/components/footer_components/Footer.tsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import '../../styles/footer/Footer.scss'
+
+export default class Footer extends React.Component {
+    constructor(props: {} | Readonly<{}>) {
+        super(props);
+    }
+
+    render() {
+        return <footer className="Footer">
+
+        </footer>;
+    }
+}
