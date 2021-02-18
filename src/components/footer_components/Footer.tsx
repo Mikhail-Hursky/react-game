3df5961aa4f72ba2c95fd5eb3fe2d531418d1e33
Mikhail Hursky <kakai747o@gmail.com>
2021-02-18 09:24:53 +0300

    feat: add Footer

--- src/components/footer_components/Footer.tsx
+++ src/components/footer_components/Footer.tsx
@@ -1,14 +1,18 @@
 import React from 'react';
 import '../../styles/footer/Footer.scss'
+import LogoCurses from '../../assets/image/rs_school_js.svg';
 
 export default class Footer extends React.Component {
-    constructor(props: {} | Readonly<{}>) {
-        super(props);
-    }
 
     render() {
         return <footer className="Footer">
+            <a href="https://rs.school/js/" target='_blank'>
+                <img src={LogoCurses} alt="curses"/>
+            </a>
 
+            <a className='author' href="https://github.com/Mikhail-Hursky" target='_blank'>by Mikhail-Hursky</a>
+
+            <p>2021</p>
         </footer>;
     }
 }
